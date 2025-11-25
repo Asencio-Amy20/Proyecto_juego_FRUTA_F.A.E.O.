7f228d11a8a71ff0d36060c8b0a939a8832a6c48
LysBorbor13 <borborlys@gmail.com>
2025-11-25 09:14:00 -0500

    Refactor Jugador class for movement and drawing

--- Fruta_Amya/jugador.py
+++ Fruta_Amya/jugador.py
@@ -3,32 +3,29 @@ import pygame
 class Jugador:
     def __init__(self, x, y):
         self.x = x
-       	self.y = y
+        self.y = y
         self.velocidad = 5
 
         try:
             imagen_original = pygame.image.load("assets/jugador/jugador.png")
             self.imagen = pygame.transform.scale(imagen_original, (60, 60))
         except:
+            # Imagen alternativa
             self.imagen = pygame.Surface((60, 60))
             self.imagen.fill((0, 0, 255))
 
-    def mover(self, teclas, velocidad=5):
-        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
-            self.x -= velocidad
-        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
-            self.x += velocidad
-        if teclas[pygame.K_UP] or teclas[pygame.K_w]:
-            self.y -= velocidad
-        if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
-            self.y += velocidad
-
-        self.x = max(0, min(self.x, 740))
-        self.y = max(0, min(self.y, 540))
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT]:
+            self.x -= self.velocidad
+        if teclas[pygame.K_RIGHT]:
+            self.x += self.velocidad
+        if teclas[pygame.K_UP]:
+            self.y -= self.velocidad
+        if teclas[pygame.K_DOWN]:
+            self.y += self.velocidad
 
     def dibujar(self, pantalla):
         pantalla.blit(self.imagen, (self.x, self.y))
 
- 
     def obtener_pos(self):
         return self.x, self.y
