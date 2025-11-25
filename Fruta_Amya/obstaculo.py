8212cbc92d63ef16364612c5980f73e06521219c
LysBorbor13 <borborlys@gmail.com>
2025-11-25 08:53:17 -0500

    arreglo3

--- Fruta_Amya/obstaculo.py
+++ Fruta_Amya/obstaculo.py
@@ -1,18 +1,19 @@
-import pygame, random, math
+import pygame
+import random
+import math
 
 class Obstaculo:
     def __init__(self):
         self.x = random.randint(100, 700)
         self.y = random.randint(100, 500)
-        self.velocidad = 2  # velocidad del tigre
+        self.velocidad = 2
 
         try:
-            imagen_original = pygame.image.load("assets/enemigo/tigre.png")
-            self.imagen = pygame.transform.scale(imagen_original, (70, 70))
+            img = pygame.image.load("assets/obstaculos/obstaculo.png")
+            self.imagen = pygame.transform.scale(img, (70, 70))
         except:
-            # Si no hay imagen, crea un cuadrado gris
             self.imagen = pygame.Surface((70, 70))
-            self.imagen.fill((100, 100, 100))
+            self.imagen.fill((200, 120, 50))
 
     def seguir_jugador(self, jugador_x, jugador_y):
         dx = jugador_x - self.x
